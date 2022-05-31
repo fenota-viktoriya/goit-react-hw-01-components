1d2a359f7c3e8544b9d4ca2277706d6f718489f3
fenota-viktoriya <Fen9206@gmail.com>
2022-05-31 17:22:44 +0300

    add files

---
+++ src/components/AppFile/App.jsx
@@ -0,0 +1,28 @@
+import user from 'data/user.json';
+import Profile from '../Users/Profile';
+import Statistics from '../Statistics/Statistics';
+import data from 'data/data.json';
+import FriendList from '../FriendList/FriendList';
+import friends from 'data/friends.json';
+import TransactionHistory from '../TransactionHistory/TransactionHistory';
+import transactions from 'data/transactions.json';
+import getRandomColor from 'helpers/getColor';
+import { Container } from './App.styled';
+
+export const App = () => {
+  return (
+    <Container>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} fn={getRandomColor} />
+      <Statistics stats={data} fn={getRandomColor} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </Container>
+  );
+};
