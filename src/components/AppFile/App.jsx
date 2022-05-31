977e4152ac0390666342bff6ea852f73f93218e6
fenota-viktoriya <Fen9206@gmail.com>
2022-05-31 17:38:05 +0300

    adds files

--- src/components/AppFile/App.jsx
+++ src/components/AppFile/App.jsx
@@ -1,5 +1,3 @@
-import Profile from '../Users/Profile';
-import user from 'data/user.json';
 import Statistics from '../Statistics/Statistics';
 import data from 'data/data.json';
 import FriendList from '../FriendList/FriendList';
@@ -8,7 +6,8 @@ import TransactionHistory from '../TransactionHistory/TransactionHistory';
 import transactions from 'data/transactions.json';
 import getRandomColor from 'helpers/getColor';
 import { Container } from './App.styled';
-
+import Profile from '../Users/Profile';
+import user from 'data/user.json';
 export const App = () => {
   return (
     <Container>
