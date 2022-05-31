906e01cc6c574ba2e4205abdeffb50519ca2d00f
fenota-viktoriya <Fen9206@gmail.com>
2022-05-31 18:41:56 +0300

    test

--- src/components/AppFile/App.jsx
+++ src/components/AppFile/App.jsx
@@ -1,5 +1,5 @@
 import Statistics from '../Statistics/Statistics';
-import data from 'data/data.json';
+import data from '../../data/data.json';
 import FriendList from '../FriendList/FriendList';
 import friends from 'data/friends.json';
 import TransactionHistory from '../TransactionHistory/TransactionHistory';
