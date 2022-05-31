e8b8a1dd801c5d264d0a644462635365fbad25d7
fenota-viktoriya <Fen9206@gmail.com>
2022-05-31 22:10:52 +0300

    rename

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/AppFile/App';
 import { Global, ThemeProvider } from '@emotion/react';
-import { GlobalStyles, theme } from 'styles-file/index';
+import { GlobalStyles, theme } from 'styles/index';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
